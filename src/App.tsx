688c4b47fc8fe8661f4f31c2757a505cebf6699b
Victor Varela <victor398@gmail.com>
2024-12-22 12:05:36 -0300

    Add header component with title for Tip Calculator app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-lime-500 text-6xl">Hola mundo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black ">Calculadora de Propinas y Consumo</h1>
+      </header>
     </>
   );
 }
